f58b1868bc09bded9faec65e28b392178e2683f4
Anish <113669810+radlysamar@users.noreply.github.com>
2022-09-29 17:29:35 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log("java script chp 3 , var let and const")
+let a = 45
+//let a = "pag"//
+a = 10
+var b = "anish"
+var c = null
+const art = "anih"
+//art = "harry"// return error because cont can't be changed
+console.log(art)
+//const ani;//initialise when you write code 
+var d = undefined
+{
+  let a = "parul"
+  console.log(a)
+}
+
+console.log(a)
